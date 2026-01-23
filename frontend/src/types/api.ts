1f3b79d0d8850a74855aed7ef664059081f8637b
Koh Yu Jun <yjenexd@gmail.com>
2026-01-23 17:42:42 +0800

    Add types for homefeed API response (POST)

--- frontend/src/types/api.ts
+++ frontend/src/types/api.ts
@@ -13,5 +13,16 @@ export interface ApiResponse<T> {
     errorCode: number;
 }
 
+export interface Post {
+  post_id: number;
+  user_id: number;
+  title: string;
+  body: string;
+  module_id: number;
+  created_at: string;
+  updated_at: string;
+  like_count: number;
+}
+
 //create a typescript interface, giving me autocomplete and type safety
 //when working with API responses that return a list of users
